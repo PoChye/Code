fadf7a3f41e036eb321f4ba57205be723e3edd7f
PoChye <90165852+PoChye@users.noreply.github.com>
2021-09-06 10:51:14 +0800

    Add files via upload

---
+++ Lab 1.2.py
@@ -0,0 +1,15 @@
+# Lab 1.2 get input two numbers and display the sum of the two numbers
+
+# Lab 1.3 get input two numbers and display the sum of the two numbers
+
+
+
+number_1 = int(input("Please enter number 1: "))
+
+number_2 = int(input("Please enter number 2: "))
+
+total = number_1+number_2
+
+print("The Sum is ",total," ",number_1,"and  ",number_2)
+
+#print("The total number is ", total)
